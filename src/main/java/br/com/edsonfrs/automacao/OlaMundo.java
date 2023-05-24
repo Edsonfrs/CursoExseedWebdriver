ff513dc9d806f8758406f8abbc5bb03056c7e8a8
Edsonfrs <edsonfsr@gmail.com>
2023-05-23 22:41:20 -0300

    Alteração da classe OlaMUndo

--- src/main/java/br/com/edsonfrs/automacao/OlaMundo.java
+++ src/main/java/br/com/edsonfrs/automacao/OlaMundo.java
@@ -3,6 +3,7 @@ package br.com.edsonfrs.automacao;
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá mundo!!!!");
+        System.out.println("Novamente...: Olá Mundo!!!!!");
 
     }
 }
